0e43aeebfb1488a7bc96164ff06f35795ac075a2
Brandon Pfeifer <thebpfeifer@gmail.com>
2017-01-18 21:37:32 -0600

    Started work on moving block based on a grid setup

--- Assets/Scripts/BlockMovement.cs
+++ Assets/Scripts/BlockMovement.cs
@@ -7,22 +7,37 @@ public class BlockMovement : MonoBehaviour {
     public Vector2 curPos;
     public float speed;
 
+    public enum Direction
+    {
+        LEFT,
+        RIGHT,
+        DOWN
+    };
+
+    private SpriteRenderer rend; 
+
 	// Use this for initialization
 	void Start () {
-        
+        rend = GetComponent<SpriteRenderer>(); 
     }
 	
 	// Update is called once per frame
 	void Update () {
         // Getting left/right input from player
         PlayerInput.x = Input.GetAxis("Horizontal");
+
         // Normalize the input so value is -1 or 1
         PlayerInput = new Vector2(PlayerInput.x, PlayerInput.y).normalized;
         // Get block's current position
         curPos = transform.position;
         // Change position based on input and speed.
-        curPos += PlayerInput * speed * Time.deltaTime;
-        // Update position
+        //curPos += PlayerInput * speed * Time.deltaTime;
+
+        //Pass the block's direction to the grid so that it can track it 
+
+
+        curPos.x += PlayerInput.x * rend.sprite.rect.size.x; 
+        //Update the position of the block by moving it the size of the block 
         transform.position = curPos;
 	}
 }
---
+++ Assets/Scripts/GridBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GridBehavior : MonoBehaviour {
+
+    public GameObject Block;
+    public int gridHeight;
+    public int gridWidth; 
+
+    private SpriteRenderer rend; 
+    private Vector2 cellSize;
+    
+    // Use this for initialization
+    void Start ()
+    {
+        rend = Block.GetComponent<SpriteRenderer>();
+        
+        //Based on the size of the block object, build a tetris grid 
+        //Get the size of our cells
+        cellSize = rend.sprite.rect.size;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+
+    void SetNewPosition(BlockMovement.Direction direction)
+    {
+
+    }
+}
