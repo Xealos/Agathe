e796ac03417001a24be417dcf58f3db019552471
Brandon Pfeifer <thebpfeifer@gmail.com>
2017-01-20 23:14:05 -0600

    Added vertical (down) block movement

--- Assets/Scripts/BlockMovement.cs
+++ Assets/Scripts/BlockMovement.cs
@@ -5,8 +5,8 @@ public class BlockMovement : MonoBehaviour {
 
     public Vector2 PlayerInput;
     public Vector2 curPos;
-    public float CooldownTime; 
-
+    public float CooldownTime_x;
+    public float CooldownTime_y; 
 
     public enum Direction
     {
@@ -16,26 +16,27 @@ public class BlockMovement : MonoBehaviour {
     };
 
     private SpriteRenderer rend;
-    private float lastTimePressed; 
+    private float lastTimePressed_x;
+    private float lastTimePressed_y; 
 
 	// Use this for initialization
 	void Start () {
         rend = GetComponent<SpriteRenderer>();
-        lastTimePressed = -100.0f; 
+        lastTimePressed_x = -100.0f;
+        lastTimePressed_y = -100.0f; 
     }
 	
 	// Update is called once per frame
 	void Update () {
         // Getting left/right input from player
         PlayerInput.x = Input.GetAxis("Horizontal");
+        PlayerInput.y = Input.GetAxis("Vertical");
 
         PlayerInput = new Vector2(PlayerInput.x, PlayerInput.y).normalized; 
 
         if( PlayerInput.x != 0 
-            && Time.time > lastTimePressed + CooldownTime )
+            && Time.time > lastTimePressed_x + CooldownTime_x )
         {
-            // Normalize the input so value is -1 or 1
-            PlayerInput = new Vector2(PlayerInput.x, PlayerInput.y).normalized;
             // Get block's current position
             curPos = transform.position;
 
@@ -49,7 +50,26 @@ public class BlockMovement : MonoBehaviour {
             //Update the position of the block by moving it the size of the block 
             transform.position = curPos;
 
-            lastTimePressed = Time.time; 
+            lastTimePressed_x = Time.time; 
+        }
+
+        if( PlayerInput.y == -1 
+            && Time.time > lastTimePressed_y + CooldownTime_y )
+        {
+            // Get block's current position
+            curPos = transform.position;
+
+            //TODO: Pass the block's direction to the grid so that it can track it
+
+            //Increment position based on the horizontal direction 
+            curPos.y += PlayerInput.y;
+
+            Debug.Log("Current position is being updated");
+
+            //Update the position of the block by moving it the size of the block 
+            transform.position = curPos;
+
+            lastTimePressed_y = Time.time;
         }
 	}
 }
