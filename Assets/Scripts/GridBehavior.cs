6c431c1a57171e7874c6def5ccacfff27f5a7f0f
TBAnamor <tbanamor@gmail.com>
2017-02-09 21:42:45 -0600

    Made GridBehavior functions

--- Assets/Scripts/GridBehavior.cs
+++ Assets/Scripts/GridBehavior.cs
@@ -48,20 +48,50 @@ public class GridBehavior : MonoBehaviour {
         blockPosition.y = y;
     }
 
-    //Clear position funciton 
+    //Clear position function 
     public void ClearPosition( int x, int y )
     {
-
+        Grid[x, y] = true;
     }
 
     //Boundary check
     public bool isMoveLegal( int x, int y, ShapeBehavior.Direction direction )
     {
+        int xNext = x, yNext = y;
+
         //Calculate requested cell position 
+        switch(direction)
+        {
+            case ShapeBehavior.Direction.DOWN:
+                xNext = x;
+                yNext = y - 1;
+                break;
+
+            case ShapeBehavior.Direction.LEFT:
+                xNext = x - 1;
+                yNext = y;
+                break;
+
+            case ShapeBehavior.Direction.RIGHT:
+                xNext = x + 1;
+                yNext = y;
+                break;
+        }
 
         //Verify requested cell position is within boundaries of game 
+        if (xNext >= gridWidth - 1
+            && xNext < 0 
+            && yNext >= gridHeight - 1 
+            && yNext < 0)
+        {
+            return false;
+        }
 
         //Verify requested cell position is not colliding with blocks 
+        if(!Grid[xNext, yNext])
+        {
+            return false;
+        }
 
         return true; 
     }
@@ -69,18 +99,46 @@ public class GridBehavior : MonoBehaviour {
     public bool rowCheck( int row )
     {
         //loop through given row and determine if all cells are occupied 
+        for(int currentColumn = 0; currentColumn < gridWidth; currentColumn++)
+        {
+            //If the space is empty
+            if(!Grid[row, currentColumn])
+            {
+                //Row is not full
+                return false;
+            }
+        }
 
+        //Row is full
         return true; 
     }
 
     public void rowClear( int row )
     {
         //clear the given row 
+        for (int currentColumn = 0; currentColumn < gridWidth; currentColumn++)
+        {
+            //Set current space to empty
+            Grid[row, currentColumn] = true;
+        }
     }
 
-    public void shiftDown( int row )
+    public void shiftDown(int row)
     {
         //shift all cells that are above the given row down 1 vertically 
+        for (int currentColumn = 0; currentColumn < gridWidth; currentColumn++)
+        {
+            //If the row is not at the bottom
+            if (row != 0)
+            {
+                //If the current position is occupied...
+                if (Grid[row, currentColumn]) {
+                    //Move it down one
+                    Grid[row - 1, currentColumn] = false;
+                    Grid[row - 1, currentColumn] = true;
+                }
+            }
+        }
     }
 
     //public bool UpdatePosition(BlockMovement.Direction direction)
